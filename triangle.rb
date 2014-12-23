93eb6c9d82d321cb8a6c1ccb1b04c9398199cd4e
chrisgrr <chrisgrr@gmail.com>
2014-12-23 22:12:11 +0000

    complete triangle project

--- triangle.rb
+++ triangle.rb
@@ -14,7 +14,13 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  if [a, b, c].uniq.length == 1
+    :equilateral
+  elsif [a, b, c].uniq.length == 2
+    :isosceles
+  else
+    :scalene
+  end
 end
 
 # Error class used in part 2.  No need to change this code.
